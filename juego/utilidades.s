35a1225f4bea9fb389a8277500f73a912c960eac
Roymar Castillo <roymarcastillo@estudiantec.cr>
2025-10-09 11:19:40 -0600

    corregir logica y constantes

--- juego/utilidades.s
+++ juego/utilidades.s
@@ -11,35 +11,38 @@ Semilla: .quad 123456789
         .extern f02LeerCadena
         .extern Buffer
 
+
 f01AsciiANumero:
-        MOV     x3, x2
-        MOV     x0, #0
-        MOV     x4, #0
+        MOV     x3, x2        
+        MOV     x0, #0        
+        MOV     x4, #0        
 
 f01AsciiANumero_loop:
         CMP     x4, x3
         B.GE    f01AsciiANumero_fin
         LDRB    w5, [x1, x4]
-        CMP     w5, #10
+        CMP     w5, #10       
         BEQ     f01AsciiANumero_fin
-        SUB     w5, w5, #'0'
-        MUL     x0, x0, #10
-        ADD     x0, x0, x5
+        SUB     w5, w5, #'0'  
+        MOV     x6, #10
+        MUL     x0, x0, x6    
+        ADD     x0, x0, x5    
         ADD     x4, x4, #1
         B       f01AsciiANumero_loop
 
 f01AsciiANumero_fin:
         RET
 
+
 f02NumeroAleatorio:
         LDR     x2, =Semilla
         LDR     x3, [x2]
-        MOV     x4, #6364136223846793005
-        MUL     x3, x3, x4
+        LDR     x4, =6364136223846793005   
         ADD     x3, x3, #1
         STR     x3, [x2]
         LSR     x0, x3, #33
         RET
+
 
 f03LeerNumero:
         BL f01ImprimirCadena
@@ -50,6 +53,7 @@ f03LeerNumero:
         MOV x2, #4
         BL f01AsciiANumero
         RET
+
 
 f04ValidarRango:
         CMP x0, x1
