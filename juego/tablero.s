d1ba8e811c150e044d10cdea462710678a505d59
Roymar Castillo <roymarcastillo@estudiantec.cr>
2025-10-09 21:58:07 -0600

    corregir errores y agregar makefile

--- juego/tablero.s
+++ juego/tablero.s
@@ -8,10 +8,6 @@ debug_iterando_fila:
         MOV x2, x0
         ADR x1, debug_tablero_filas
         BL f01ImprimirCadena
-        ADR x1, NuevaLinea
-        ADR x2, LargoNuevaLineaVal
-        LDR x2, [x2]
-        BL f01ImprimirCadena
         MOV x0, x0
         BL print_decimal
         // DEPURACION: Imprimir valor de columnas
@@ -19,10 +15,6 @@ debug_iterando_fila:
         BL f05LongitudCadena
         MOV x2, x0
         ADR x1, debug_tablero_columnas
-        BL f01ImprimirCadena
-        ADR x1, NuevaLinea
-        ADR x2, LargoNuevaLineaVal
-        LDR x2, [x2]
         BL f01ImprimirCadena
         MOV x0, x1
         BL print_decimal
@@ -32,29 +24,17 @@ debug_iterando_fila:
         MOV x2, x0
         ADR x1, debug_antes_menu_acciones
         BL f01ImprimirCadena
-        ADR x1, NuevaLinea
-        ADR x2, LargoNuevaLineaVal
-        LDR x2, [x2]
-        BL f01ImprimirCadena
         // DEPURACION: Antes de imprimir tablero
         ADR x1, debug_antes_imprimir_tablero
         BL f05LongitudCadena
         MOV x2, x0
         ADR x1, debug_antes_imprimir_tablero
         BL f01ImprimirCadena
-        ADR x1, NuevaLinea
-        ADR x2, LargoNuevaLineaVal
-        LDR x2, [x2]
-        BL f01ImprimirCadena
         // DEPURACION: Después de imprimir tablero
         ADR x1, debug_despues_imprimir_tablero
         BL f05LongitudCadena
         MOV x2, x0
         ADR x1, debug_despues_imprimir_tablero
-        BL f01ImprimirCadena
-        ADR x1, NuevaLinea
-        ADR x2, LargoNuevaLineaVal
-        LDR x2, [x2]
         BL f01ImprimirCadena
         .section .bss
 Tablero:
